e8b261f3e7217124a671e471e6d91afa774c4faf
Copenhagen <1903373681@qq.com>
2019-04-06 13:40:25 +0800

    v2.2
    修改

--- src/main/java/top/esmart/web/coreServlet.java
+++ src/main/java/top/esmart/web/coreServlet.java
@@ -24,7 +24,7 @@ public class coreServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("jenkins Served at: ").append(request.getContextPath());
 	}
 
 	/**
